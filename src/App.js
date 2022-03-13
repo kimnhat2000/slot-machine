a6310cee7dc1f916025d7fc432efed632736cee7
Nhat Nguyen <kimnhat2000@gmail.com>
2022-03-13 19:07:01 -0400

    rewrite first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import SlotNum from "./components/SlotNum";
+import React, {Component} from "react"
 
-function App() {
-  onplay = () => {
-    let arr = [1, 1, 2, 1];
-    console.log(arr.every((val, i, arr) => val === arr[0]));
-  };
-  return (
-    <div>
-      <SlotNum />
-      <button onClick={onplay}>Roll</button>
-    </div>
-  );
+import {AiOutlinePlus, AiFillCaretUp, AiFillCaretDown} from "react-icons/ai"
+import {icons} from "./iconRender/icons"
+
+ 
+
+class App extends Component {
+  constructor (props){
+    super (props)
+    this.state={
+      cardNum:2,
+      ranNum:[],
+      message:"",
+    }
+
+  }
+  clickThisButton=()=>{
+    randomCardArray(2,2)
+    console.log(randomCardArray())
+  }
+  render (){
+    const style={
+      backgroundColor : "gray",
+      height: "100px",
+      width:"100px",
+      boxShadow: "5px 5px 15px 5px #000000",
+    }
+    return(
+      <div>
+        <AiOutlinePlus style={style}/>
+        <button onClick={()=>clickThisButton()}><AiFillCaretUp/></button>
+        <button><AiFillCaretDown/></button>
+      </div>
+      
+    )
+  }
 }
 
+
 export default App;
+
+function randomCardArray (cardNum, ranNumRange) {
+  let ranNum=[]
+  let range = Math.floor(Math.random()*ranNumRange)
+  for (let i=0; i<cardNum; i++){
+    ranNum.push(range)
+  }
+  return ranNum;
+}
